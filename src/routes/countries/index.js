0a95987ff76c9d29506e17ff109baf74e3becfb1
Tadeo Daniel <tadeo@MacBook-Pro-de-Tadeo.local>
2022-09-19 20:53:37 -0500

    othr: new flow for endpoints

--- src/routes/countries/index.js
+++ src/routes/countries/index.js
@@ -1,59 +1,69 @@
 const express = require('express');
 const router = express.Router();
-
-const mysqlConnection = require('../../db');
+const axios = require('axios');
 const { buildResponse } = require('../../utils/functions');
 
+const countries = {
+  Europe: 'http://3.101.113.126/Server-Europe-Continent/index.php',
+  Africa: 'http://3.85.32.230/Server-Africa-Continent/index.php',
+  'North America': 'http://34.226.202.66/server-america-continent/index.php',
+  'South America': 'http://34.226.202.66/server-america-continent/index.php',
+};
+
+const capitalize = (str) => {
+  const lower = str.toLowerCase();
+  return lower.replace(/(^\w{1})|(\s+\w{1})/g, letter => letter.toUpperCase());
+};
+
 // GET all countries 
-router.get('/countries', (req, res) => {
-  mysqlConnection.query('SELECT * FROM countries', (err, rows) => {
-    if (!err) {
-      res.json(
-        buildResponse(
-          'Success',
-          200,
-          'The countries resource has been loaded successfully.',
-          rows,
-        )
-      );
-    } else {
-      res.json(
-        buildResponse(
-          'Error',
-          500,
-          'An error ocurred while getting the resource.',
-        )
-      );
-    }
-  });
+router.get('/countries', async(req, res) => {
+  try {
+    const { name } = req.query;
+    const continent = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
+    const response = await axios.get(`${countries[continent?.data[0]?.continents[0]]}?Name=${name}`);
+    res.json(
+      buildResponse(
+        'Success',
+        200,
+        'The countries resource has been loaded successfully.',
+        response?.data,
+      )
+    );
+  } catch (e) {
+    const { response : { data }} = e
+    res.json(
+      buildResponse(
+        'Error',
+        data.status,
+        data.message,
+      )
+    );
+  }
 });
 
-// GET a single country by name
-router.get('/country', (req, res) => {
-  const { name } = req.query;
-  
-  mysqlConnection.query('SELECT * FROM countries WHERE Name = ?', [name], (err, rows) => {
-    if (!err) {
-      res.json(
-        buildResponse(
-          'Success',
-          200,
-          'The country resource has been loaded successfully.',
-          rows[0],
-        )
-      );
-    } else {
-      res.json(
-        buildResponse(
-          'Error',
-          500,
-          'An error ocurred while getting the resource.',
-        )
-      );
-    }
-  });
+// GET all countries 
+router.get('/countries-by-continent', async(req, res) => {
+  try {
+    const { continent } = req.query;
+    const response = await axios.get(`${countries[capitalize(continent)]}`);
+    res.json(
+      buildResponse(
+        'Success',
+        200,
+        'The countries resource has been loaded successfully.',
+        response?.data,
+      )
+    );
+  } catch (e) {
+    res.json(
+      buildResponse(
+        'Error',
+        404,
+        'Not found',
+      )
+    );
+
+  }
 });
-
-
 
 module.exports = router;
